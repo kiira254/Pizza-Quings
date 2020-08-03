4eaf8cc644fed2ae14155358f0c0561d18ce72d3
nelly kamotho <nkamothO57@gmail.com>
2020-08-03 06:48:50 +0300

    add input

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+(function () {
+    $('.summary').hide();
+    $('.cdata-overlay').hide();
+    //Get inputs
+    $("#checkout").click(function () {
+        let flavour = $(".flavour option:selected").val();
+        let size = $("#size option:selected").val();
+        let crust = $("#crust option:selected").val();
+        let topping = $("#toppings option:selected").val();
+        let number = $("#number").val();
+        console.log(size);
