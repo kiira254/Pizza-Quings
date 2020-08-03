89fe8d0bdf01f0e409e9fad362f4624f5950c659
nelly kamotho <nkamothO57@gmail.com>
2020-08-03 06:54:06 +0300

    add pick up

--- js/scripts.js
+++ js/scripts.js
@@ -395,14 +395,14 @@
       + newOrder.t + "<br>" + " Number of pizzas :    "
       + newOrder.n + "<br>" + "Total Price :  "
       + newOrder.total + "<br><br>").css('font-family', 'system-ui').css('font-size', '24px');
-});
- //Deliver
- $(".deliver").click(function () {
-    $('.summary').slideUp();
-    $('#list').slideUp();
-    $('.summary').text("Provide location details").slideDown();
-    $('.deliver').hide(1000);
-    $('.delivernot').hide(1000);
-    $('.cdata-overlay').slideDown();
-});
-
+    });
+    //Deliver
+    $(".deliver").click(function () {
+        $('.summary').slideUp();
+        $('#list').slideUp();
+        $('.summary').text("Provide location details").slideDown();
+        $('.deliver').hide(1000);
+        $('.delivernot').hide(1000);
+        $('.cdata-overlay').slideDown();
+    });
+})
